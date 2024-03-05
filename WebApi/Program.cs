839134bcdd5544f33b3053986e366c936780c399
@mohamad66 <rahimi@homacall.com>
2024-03-05 22:03:48 +0330

    yuu

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowOrigin",
         builder => builder
          
-          .WithOrigins("http://localhost:3000")
+          .WithOrigins("http://localhost:3000", "http://localhost:80/api")
           .AllowAnyMethod()
           .AllowAnyHeader()
           .AllowCredentials()
